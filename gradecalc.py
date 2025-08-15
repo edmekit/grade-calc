ab7aa1e966d2a234075d885b2e466ea694d5aae5
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2025-08-16 00:31:02 +0800

    files

--- gradecalc.py
+++ gradecalc.py
@@ -3,20 +3,20 @@ over = int(input("How many items? "))
 grade = []
 overall = []
 
-with open("scores.txt", "a") as f:
+with open("scores/mathscores.txt", "a") as f:
     f.write(str(scores) + "\n")
 
-with open("scores.txt", "r") as f:
+with open("scores/mathscores.txt", "r") as f:
     grades = f.readlines()
     for gradee in grades:
         gradee = gradee.strip()
         if gradee:
             grade.append(int(gradee))
 
-with open("over.txt", "a") as f:
+with open("scores/mathover.txt", "a") as f:
     f.write(str(over) + "\n")
     
-with open("over.txt", "r") as f:
+with open("scores/mathover.txt", "r") as f:
     overs = f.readlines()
     for total in overs:
         total = total.strip()
